8c2011be84347756c907873def5b1f364ca50fc7
鈦匕工作室 - 貓虎皮 <60348735+MaoHuPi@users.noreply.github.com>
2022-06-26 17:19:01 +0800

    Create color.py

---
+++ canvas/color.py
@@ -0,0 +1,17 @@
+class color:
+    def __init__(self):
+        self.darkBrown = (113, 71, 0)
+        self.black = (0, 0, 0)
+        self.white = (255, 255, 255)
+        self.orange = (255, 214, 128)
+        self.dot_color = {}
+        self.dot_color['red'] = (224, 102, 102)
+        self.dot_color['orange'] = (246, 178, 107)
+        self.dot_color['yellow'] = (255, 217, 102)
+        self.dot_color['green'] = (147, 196, 125)
+        self.dot_color['darkslategray'] = (118, 165, 175)
+        self.dot_color['skyblue'] = (109, 158, 235)
+        self.dot_color['blue'] = (111, 168, 220)
+        self.dot_color['purple'] = (142, 124, 195)
+        self.dot_color['deepwine'] = (194, 123, 160)
+        self.dot_color['brown'] = (204, 65, 37)
